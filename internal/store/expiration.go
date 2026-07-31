13750c381b42cdd53730e0a782d0c2eb7c3d4cfb
devanshu0x <devanshudotdev@gmail.com>
2026-07-31 23:09:30 +0530

    fix(store): update active deletion for hashed table

--- internal/store/expiration.go
+++ internal/store/expiration.go
@@ -1,49 +1,56 @@
-// package store
+package store
 
-// import (
-// 	"log"
-// 	"time"
-// )
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
-// func expireSample() float32{
-// 	limit:=20
-// 	expiredCount:=0
+func expireSample() float64 {
+	limit:=min(20,store.ht[0].used)
+	expiredCount:=0
+	sampled:=0
 
-// 	// assuming iteration in golang hash table is randomized
+	if limit<=0{
+		return 0
+	}
 
-// 	for key,obj:= range store{
-// 		if obj.ExpiresAt==-1{
-// 			continue
-// 		}
-// 		// Current obj do have some ttl value not -1
-// 		limit--
-// 		if obj.ExpiresAt<=time.Now().UnixMilli(){
-// 			delete(store,key)
-// 			expiredCount++;
-// 		}
+	for sampled<limit{
+		idx:=rand.Intn(len(store.ht[0].buckets))
 
-// 		if limit==0{
-// 			break
-// 		}
-// 	}
-// 	sampled:=20-limit
-// 	if sampled==0{
-// 		return 0
-// 	}
-// 	return  float32(expiredCount)/float32(sampled)
-// }
+		curr:=store.ht[0].buckets[idx]
+
+		for curr!=nil && sampled<limit{
+			sampled++
+			obj:=curr.Obj
+			next:=curr.Next
+			if obj.ExpiresAt!=-1 && obj.ExpiresAt<=time.Now().UnixMilli(){
+				store.ht[0].deleteEntry(curr.Key)
+				expiredCount++
+			}
+
+			curr=next
+		}
+	}
+
+	return float64(expiredCount)/float64(sampled)
+}
 
 
-// // active deletion: delete all the expired key
-// func DeleteExpiredKeys(){
-// 	for{
-// 		// if the sample had less than 25% keys expired
-// 		frac:=expireSample()
+// active deletion: delete all the expired key
+func DeleteExpiredKeys(){
+	// skip active deletion during rehashing
+	if store.isRehashing(){
+		return
+	}
+	for{
+		// if the sample had less than 25% keys expired
+		frac:=expireSample()
 
-// 		if frac<0.25{
-// 			break
-// 		}
-// 	}
+		if frac<0.25{
+			break
+		}
+	}
 
-// 	log.Println("Deleted expired but undeleted keys. Total keys left: ",len(store))
-// }
+	log.Println("Deleted expired but undeleted keys. Total keys left: ",store.ht[0].used)
+}
