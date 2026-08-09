e036bebad0050c9b4bc80e863d7da21821687895
devanshu0x <devanshudotdev@gmail.com>
2026-08-10 01:36:02 +0530

    test: add tests for rdb

---
+++ internal/persistence/rdb_test.go
@@ -0,0 +1,47 @@
+package persistence
+
+import (
+	"os"
+	"testing"
+
+	"github.com/devanshu0x/Aster/internal/store"
+)
+
+func TestRDB(t *testing.T) {
+	snapshot := store.Snapshot{
+		Entries: []store.SnapshotEntry{
+			{
+				Key:       "foo",
+				Type:      store.StringObject,
+				Value:     "bar",
+				ExpiresAt: -1,
+			},
+			{
+				Key:       "hello",
+				Type:      store.StringObject,
+				Value:     "world",
+				ExpiresAt: 123456789,
+			},
+		},
+	}
+
+	path := "test.rdb"
+
+	err := SaveRDB(snapshot, path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(path)
+
+	loaded, err := LoadRDB(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(loaded.Entries) != 2 {
+		t.Fatalf("expected 2 entries, got %d", len(loaded.Entries))
+	}
+
+	t.Logf("%+v", loaded)
+}
